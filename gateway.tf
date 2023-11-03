19510597564d740e629508cbbf3df74edb400746
emirhan <emir_849@outlook.com>
2023-11-02 20:52:13 -0400

    project

--- gateway.tf
+++ gateway.tf
@@ -6,8 +6,3 @@ resource "aws_internet_gateway" "my_igw" {
   }
 }
 
-# Attach the Internet Gateway to the VPC
-resource "aws_vpc_attachment" "my_vpc_attachment" {
-  vpc_id             = aws_vpc.my_vpc.id
-  internet_gateway_id = aws_internet_gateway.my_igw.id
-}
--- lb.tf
+++ lb.tf
@@ -6,41 +6,6 @@ resource "aws_lb" "my_alb" {
   subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
 }
 
-# Create /path rules for the ALB
-resource "aws_lb_listener_rule" "itmeme" {
-  listener_arn = aws_lb.my_alb.arn
-  action {
-    type             = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      status_code  = "200"
-      content      = "Images"
-    }
-  }
-
-  condition {
-    path_pattern {
-      values = ["/itmeme/*"]
-    }
-  }
-}
-
-resource "aws_lb_listener_rule" "video" {
-  listener_arn = aws_lb.my_alb.arn
-  action {
-    type             = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      status_code  = "200"
-      content      = "Videos"
-    }
-  }
-  condition {
-    path_pattern {
-      values = ["/video/*"]
-    }
-  }
-}
 
 # Create two Target Groups
 resource "aws_lb_target_group" "images_tg" {
