4d44161ede997448d7d308f40755bf7637b23b3b
emirhan <emir_849@outlook.com>
2023-11-04 00:59:27 -0400

    project

--- gateway.tf
+++ gateway.tf
@@ -6,3 +6,11 @@ resource "aws_internet_gateway" "my_igw" {
   }
 }
 
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+}
