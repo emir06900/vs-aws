bb884e82452894e420fd2d03cfee1396455fcdb7
emirhan <emir_849@outlook.com>
2023-11-04 01:09:38 -0400

    project

--- lb.tf
+++ lb.tf
@@ -23,7 +23,7 @@ resource "aws_lb_target_group" "videos_tg" {
 }
 
 resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.my_lb.arn
+  load_balancer_arn = aws_lb.my_alb.arn
   port              = 80
   protocol          = "HTTP"
 
