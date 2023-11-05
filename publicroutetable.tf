53aa421bfc908040e34a9f10801b9083700a4cb1
emirhan <emir_849@outlook.com>
2023-11-04 20:05:23 -0400

    project

--- publicroutetable.tf
+++ publicroutetable.tf
@@ -4,9 +4,9 @@ resource "aws_route_table" "public_rt" {
   tags = {
     Name = "project"
   }
+}
 resource "aws_route" "public_route" {
   route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.my_igw.id
-}
 }
