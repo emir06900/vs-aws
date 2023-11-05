1bd21c5643fcdac7bfa147e2b3744520c41cc010
emirhan <emir_849@outlook.com>
2023-11-04 20:33:21 -0400

    project

--- publicroutetable.tf
+++ publicroutetable.tf
@@ -10,3 +10,12 @@ resource "aws_route" "public_route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.my_igw.id
 }
+resource "aws_route_table_association" "public_subnet_association1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_subnet_association2" {
+  subnet_id      = aws_subnet.public_subnet2.id
+  route_table_id = aws_route_table.public_rt.id
+}
