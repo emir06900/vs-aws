24c1a34955a46fe399af5e626569489600c5381c
emirhan <emir_849@outlook.com>
2023-11-09 23:34:07 -0500

    project

--- vpc.tf
+++ vpc.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24" # Adjust the CIDR block as needed
-  availability_zone       = "us-east-1a"  # Adjust the availability zone
+  availability_zone       = "us-east-1b"  # Adjust the availability zone
   map_public_ip_on_launch = true
   
   tags = {
@@ -22,7 +22,7 @@ resource "aws_subnet" "public_subnet_1" {
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.2.0/24" # Adjust the CIDR block as needed
-  availability_zone       = "us-east-1b"  # Adjust the availability zone
+  availability_zone       = "us-east-1a"  # Adjust the availability zone
   map_public_ip_on_launch = true
 
   tags = {
